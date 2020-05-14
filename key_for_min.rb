a86c3c60114b7e4b07bff45dcf622bde2c5a20ca
Robert Lopez <robert.lopez@yale.edu>
2020-05-14 23:33:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-   a_value = Float::INFINITY
+   min_value = Float::INFINITY
    min_key = ""
   name_hash.each do |key, value|
-    if value < a_value do 
-      a_value = item
+    if value < min_value 
+      min_value = value 
       min_key = key 
-  end 
 end
 end 
 min_key  
