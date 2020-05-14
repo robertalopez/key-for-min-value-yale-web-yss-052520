d42fba8a196fd0a123b10a0fcccc165d38cd1adb
Robert Lopez <robert.lopez@yale.edu>
2020-05-14 23:30:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
    min_value = Float::INFINITY
    min_key = ""
   name_hash.each do |key, value|
-    if valie < min_value do 
+    if value < min_value do 
       min_value = item
       min_key = key 
   end 
