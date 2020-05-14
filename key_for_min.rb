89bbdbe38d7f54e8bf9cee787f76df31d5b6f5c0
Robert Lopez <robert.lopez@yale.edu>
2020-05-14 23:24:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
   name_hash.each do |item|
     min_value = Float::INFINITY 
     if item < min_value do 
